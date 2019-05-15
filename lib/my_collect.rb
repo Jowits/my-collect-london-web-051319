703566a8596e39b55a91b93e11a99296a8c130f1
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-15 13:00:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(empty_array)
   count = 0
   array = []
   while count < empty_array.length
-    array << yield (empty_array[i])
+    array << yield(empty_array[i])
     count += 1
   end
   return array
